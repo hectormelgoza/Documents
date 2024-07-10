88255c331928839a238375bb09dbafb8c9d264d4
hectormelgoza <42985306+hectormelgoza@users.noreply.github.com>
2024-07-10 14:52:02 -0700

    original file

---
+++ price_change_option_change.js
@@ -0,0 +1,28 @@
+
+$(function() {if ($('.product-info [name=\'quantity\']').val() > 1) getLatestprice()});
+$(document).on('change','[name^=\'option[\']', getLatestprice);
+$(document).on('keyup','[name=\'quantity\']', function() {
+	if (!isNaN(this.value) && this.value > 0) getLatestprice();
+});
+var alreadyExec;
+function getLatestprice() {
+
+	if (alreadyExec) alreadyExec.abort();
+
+	alreadyExec = $.ajax({
+		url: 'index.php?route=module/price_change_option_change/getLatestPriceprice',
+		type: 'post',
+		data: $('.product-info [name=\'quantity\'],.product-info [type=\'radio\']:checked, .product-info [type=\'checkbox\']:checked,  .product-info select, .product-info [name=\'product_id\']'),
+		dataType: 'json',
+		success: function(response) {
+
+			$('.price-container').text(response.price);
+			$('.price-old').text(response.price);
+            $('.price-normal').text(response.price);
+			$('.price-new').text(response.special);
+			$('.price-tax span').text(response.ex_tax);
+			$('.reward span').text(response.points);
+
+		}
+	});
+}
