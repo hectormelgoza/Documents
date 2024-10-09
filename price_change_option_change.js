fe29e6edc63e9ecd34bb6d1f32f89ab58c76d8b1
hectormelgoza <42985306+hectormelgoza@users.noreply.github.com>
2024-10-09 15:05:22 -0700

    added total units count

--- price_change_option_change.js
+++ price_change_option_change.js
@@ -17,9 +17,16 @@ $(document).on('click', '.qty-minus', function() {
     }
 });
 
+$(document).ready(function() {
+    // Click event handler for close button within #notification
+    $('#notification').on('click', '.close', function() {
+        $(this).closest('.success').fadeOut();
+    });
+});
 
 
 var alreadyExec;
+
 function getLatestprice() {
 
 	if (alreadyExec) alreadyExec.abort();
@@ -30,6 +37,12 @@ function getLatestprice() {
 		data: $('.product-info [name=\'quantity\'],.product-info [type=\'radio\']:checked, .product-info [type=\'checkbox\']:checked,  .product-info select, .product-info [name=\'product_id\']'),
 		dataType: 'json',
 		success: function(response) {
+            const multiplier = parseFloat($('.multiplier').text()) || 1; // Default to 1 if not found
+            const quantity = parseFloat($('.product-info [name="quantity"]').val()) || 1; // Default to 1 if quantity is not valid
+            const totalUnits = multiplier * quantity;
+
+            const formattedTotalUnits = totalUnits.toLocaleString();
+            const unitText = totalUnits === 1 ? 'unit' : 'units'; // Determine singular/plural
 
 			$('.price-container').text(response.price);
 			$('.price-old').text(response.price);
@@ -37,6 +50,7 @@ function getLatestprice() {
 			$('.price-new').text(response.special);
 			$('.price-tax span').text(response.ex_tax);
 			$('.reward span').text(response.points);
+            $('.total-units').text(`(${formattedTotalUnits} ${unitText})`);
 
 		}
 	});
